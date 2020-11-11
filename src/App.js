3f243496f030e33a594d428fe677fd24dc3195d3
Oleg <justdoomed@gmail.com>
2020-11-11 19:17:33 +0200

    Swipe support added

--- src/App.js
+++ src/App.js
@@ -18,10 +18,49 @@ function App() {
         openedMenu = !openedMenu;
     };
 
+    // handle swipe
+    let xDown = null;
+    let yDown = null;
+
+    const handleTouchStart = e => {
+        const firstTouch = e.touches[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    };
+
+    const handleTouchMove = e => {
+        if ( ! xDown || ! yDown ) {
+            return;
+        }
+
+        let xUp = e.touches[0].clientX;
+        let yUp = e.touches[0].clientY;
+
+        let xDiff = xDown - xUp;
+        let yDiff = yDown - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+            if ( xDiff > 0 ) {
+                openedMenu = false;
+                document.getElementById('content').classList.remove('open');
+            } else {
+                openedMenu = false;
+                document.getElementById('content').classList.add('open');
+            }
+        }
+
+        xDown = null;
+        yDown = null;
+    };
+
+
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+
     return (
         <div className="App">
             <Menu toggleMenu={menuHandler} />
-            <div className="content" ref={contentDiv}>
+            <div className="content" id="content" ref={contentDiv}>
                 <Content toggleMenu={menuHandler} />
             </div>
         </div>
